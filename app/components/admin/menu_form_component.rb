9c58fa52cc3da17de1723a50b66479e076f6b988
Thai Dang <thai.hulk@gmail.com>
2023-04-09 17:50:19 +0700

    remove admin components

--- app/components/admin/menu_form_component.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-module Admin
-  class MenuFormComponent < ViewComponent::Base
-  end
-end
--- app/models/featured.rb
+++ app/models/featured.rb
@@ -4,4 +4,6 @@ class Featured < ApplicationRecord
   belongs_to :article
 
   validates :position, numericality: { only_integer: true }, allow_blank: true
+
+  delegate :title, :slug, to: :article, allow_nil: true
 end
