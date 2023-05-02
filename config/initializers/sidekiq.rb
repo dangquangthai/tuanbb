8496f545831981fa1a2c27681c955ed157b91e01
Thai Dang <thai.hulk@gmail.com>
2023-05-02 18:35:59 +0700

    fix sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,13 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://127.0.0.1:6379/12", network_timeout: 5 }
-end
 
-Sidekiq.configure_client do |config|
-  config.redis = { url: "redis://127.0.0.1:6379/12", network_timeout: 5 }
+  # edits the default capsule
+  config.queues = %w[critical default low]
+  config.concurrency = 5
+
+  # define a new capsule which processes jobs from the `unsafe` queue one at a time
+  config.capsule("unsafe") do |cap|
+    cap.concurrency = 1
+    cap.queues = %w[unsafe]
+  end
 end
